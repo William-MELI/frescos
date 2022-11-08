3c8935c502dca3292355a9dc7e4ca69af8152869
illpereira <william.pereira@mercadolivre.com>
2022-11-08 16:01:06 -0300

    feat: Configurando banco de dados.

--- src/main/java/com/meli/frescos/FreshApplication.java
+++ src/main/java/com/meli/frescos/FreshApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FrescosApplication {
+public class FreshApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FrescosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FreshApplication.class, args);
+    }
 
 }
