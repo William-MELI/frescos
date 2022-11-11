3085b0de2b170f62e79fd0406e654e86bd27ffba
emicarvalho <emily.carvalho@mercadolivre.com>
2022-11-11 18:26:18 -0300

    docs: documentacao das model, OrderProductsController e OrderProductService

--- src/main/java/com/meli/frescos/model/ProductModel.java
+++ src/main/java/com/meli/frescos/model/ProductModel.java
@@ -6,6 +6,9 @@ import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+/**
+ * Main Product Entity
+ */
 @Entity
 @Getter
 @Setter
@@ -15,25 +18,63 @@ import java.time.LocalDate;
 @Table(name = "product")
 public class ProductModel {
 
+    /**
+     * Product ID
+     * Auto-generated
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * Product productTitle
+     * Not Nullable
+     */
+    @Column(nullable = false)
     private String productTitle;
 
+    /**
+     * Product description
+     * Not Nullable
+     */
+    @Column(nullable = false)
     private String description;
 
+    /**
+     * Product price
+     * Not Nullable
+     */
+    @Column(nullable = false)
     private BigDecimal price;
 
     @Enumerated(EnumType.STRING)
     private CategoryEnum category;
 
+    /**
+     * Product unitVolume
+     * Not Nullable
+     */
+    @Column(nullable = false)
     private Double unitVolume;
 
+    /**
+     * Product unitWeight
+     * Not Nullable
+     */
+    @Column(nullable = false)
     private Double unitWeight;
 
+    /**
+     * Product createDate
+     * Not Nullable
+     */
+    @Column(nullable = false)
     private LocalDate createDate;
 
+    /**
+     * Seller reference.
+     * It is an N-1 relationship
+     */
     @ManyToOne
     private SellerModel seller;
 }
