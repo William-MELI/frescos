d60c5c5cf417eb00294061b04b0bf1596ef21468
MLjoaolaureano <joao.laureano@mercadolivre.com>
2022-11-14 18:32:57 -0300

    fix:Alterando classe de exception para extender Exception

--- src/main/java/com/meli/frescos/exception/WarehouseNotFoundException.java
+++ src/main/java/com/meli/frescos/exception/WarehouseNotFoundException.java
@@ -1,6 +1,6 @@
 package com.meli.frescos.exception;
 
-public class WarehouseNotFoundException extends RuntimeException {
+public class WarehouseNotFoundException extends Exception {
     public WarehouseNotFoundException(String s) {
         super(s);
     }
