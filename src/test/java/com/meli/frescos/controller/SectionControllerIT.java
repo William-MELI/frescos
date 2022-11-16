48050eaa94def4fcefcc31f3edc5e13d3f85428f
emicarvalho <emily.carvalho@mercadolivre.com>
2022-11-16 17:34:26 -0300

    test: teste integracao Section

--- src/test/java/com/meli/frescos/controller/SectionControllerIT.java
+++ src/test/java/com/meli/frescos/controller/SectionControllerIT.java
@@ -10,7 +10,6 @@ import com.meli.frescos.repository.IWarehouseRepository;
 import com.meli.frescos.repository.SectionRepository;
 import com.meli.frescos.service.ISectionService;
 import com.meli.frescos.service.IWarehouseService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +19,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.testng.annotations.BeforeClass;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -47,44 +47,16 @@ public class SectionControllerIT {
     @Autowired
     private SectionRepository sectionRepo;
 
-    @BeforeEach
-    void setup() {
-        this.sectionRepo.deleteAll();
-        this.warehouseRepo.deleteAll();
-    }
-
     private void newWarehouseRecord() {
         WarehouseModel newWarehouse = new WarehouseModel("Tramandaí", "Rio Grande do Sul", "Avenida Emancipacao", "99999999", "Zona Sul");
         warehouseService.save(newWarehouse);
     }
 
-
-    @Test
-    @DisplayName("Test Section Successfull Creation - POST Endpoint")
-    void create_returnsCreatedStatus_whenSuccess()  throws Exception {
-        newWarehouseRecord();
-        String description = "marca";
-        CategoryEnum category = CategoryEnum.FRESH;
-        Double totalSize = 40.0;
-        Double temperature = 4.8;
-        Long warehouse = 1L;
-
-        SectionRequest sectionRequest = SectionRequest.builder()
-                .description(description)
-                .category(category)
-                .totalSize(totalSize)
-                .temperature(temperature)
-                .warehouse(warehouse)
-                .build();
-
-        ResultActions response = mockMvc.perform(
-                post("/section")
-                        .content(objectMapper.writeValueAsString(sectionRequest))
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
-        response.andExpect(status().isCreated());
+    @BeforeClass
+    void setup() {
+        this.sectionRepo.deleteAll();
+        this.warehouseRepo.deleteAll();
     }
-
 
     @Test
     @DisplayName("Test Get Section by ID - GET Endpoint")
@@ -110,7 +82,7 @@ public class SectionControllerIT {
                 get("/section/{id}", String.valueOf(section.getId()))
                         .contentType(MediaType.APPLICATION_JSON)
         );
-        response.andExpect(status().isFound());
+        response.andExpect(status().isOk());
     }
 
     @Test
@@ -153,6 +125,33 @@ public class SectionControllerIT {
                         .contentType(MediaType.APPLICATION_JSON)
         );
 
-        response.andExpect(status().isFound());
+        response.andExpect(status().isOk());
+    }
+
+
+    @Test
+    @DisplayName("Test Section Successfull Creation - POST Endpoint")
+    void create_returnsCreatedStatus_whenSuccess()  throws Exception {
+        newWarehouseRecord();
+        String description = "marca";
+        CategoryEnum category = CategoryEnum.FRESH;
+        Double totalSize = 40.0;
+        Double temperature = 4.8;
+        Long warehouse = 1L;
+
+        SectionRequest sectionRequest = SectionRequest.builder()
+                .description(description)
+                .category(category)
+                .totalSize(totalSize)
+                .temperature(temperature)
+                .warehouse(warehouse)
+                .build();
+
+        ResultActions response = mockMvc.perform(
+                post("/section")
+                        .content(objectMapper.writeValueAsString(sectionRequest))
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+        response.andExpect(status().isCreated());
     }
 }
