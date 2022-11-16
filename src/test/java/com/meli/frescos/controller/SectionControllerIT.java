5593bf7ab7098341affdc2212f9087bc9f110cae
emicarvalho <emily.carvalho@mercadolivre.com>
2022-11-16 10:09:23 -0300

    test: teste de integracao para pegar um Setor pelo Id

--- src/test/java/com/meli/frescos/controller/SectionControllerIT.java
+++ src/test/java/com/meli/frescos/controller/SectionControllerIT.java
@@ -2,7 +2,9 @@ package com.meli.frescos.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.meli.frescos.controller.dto.SectionRequest;
+import com.meli.frescos.exception.SectionByIdNotFoundException;
 import com.meli.frescos.model.CategoryEnum;
+import com.meli.frescos.model.SectionModel;
 import com.meli.frescos.model.WarehouseModel;
 import com.meli.frescos.repository.IWarehouseRepository;
 import com.meli.frescos.repository.SectionRepository;
@@ -19,7 +21,8 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -37,6 +40,9 @@ public class SectionControllerIT {
 
     @Autowired
     private IWarehouseRepository warehouseRepo;
+
+    @Autowired
+    private ISectionService sectionService;
 
     @Autowired
     private SectionRepository sectionRepo;
@@ -81,8 +87,9 @@ public class SectionControllerIT {
 
 
     @Test
-    @DisplayName("Test Section creating - POST Endpoint")
-    void create_throwException_whenWarehouseNotFound() throws Exception {
+    @DisplayName("Test Get Section by ID - GET Endpoint")
+    void getById_returnsSection_whenIdIsAvailable() throws Exception {
+        newWarehouseRecord();
         String description = "marca";
         CategoryEnum category = CategoryEnum.FRESH;
         Double totalSize = 40.0;
@@ -97,11 +104,55 @@ public class SectionControllerIT {
                 .warehouse(warehouse)
                 .build();
 
+        SectionModel section = sectionService.save(sectionRequest);
+
         ResultActions response =  mockMvc.perform(
-                post("/section")
-                        .content(objectMapper.writeValueAsBytes(sectionRequest))
+                get("/section/{id}", String.valueOf(section.getId()))
                         .contentType(MediaType.APPLICATION_JSON)
         );
-        response.andExpect(status().isNotFound());
+        response.andExpect(status().isFound());
+    }
+
+    @Test
+    void getById_throwSectionByIdNotFoundException_whenInexistentSection() throws Exception {
+        ResultActions response = mockMvc.perform(
+                get("/section/{id}", Long.MAX_VALUE)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+        response.andExpect(status().isNotFound())
+                .andExpect(
+                        result -> assertTrue(
+                                result.getResolvedException() instanceof SectionByIdNotFoundException)
+                );
+    }
+
+    @Test
+    void getAll_returnListOfSection_whenSuccess() throws Exception {
+        newWarehouseRecord();
+        SectionRequest newSectionRequestUm  = SectionRequest.builder()
+                .description("marca")
+                .category(CategoryEnum.FROZEN)
+                .totalSize(40.0)
+                .temperature(4.8)
+                .warehouse(1L)
+                .build();
+
+        SectionRequest newSectionRequestDois  = SectionRequest.builder()
+                .description("Produtos frescos")
+                .category(CategoryEnum.FRESH)
+                .totalSize(50.2)
+                .temperature(12.5)
+                .warehouse(1L)
+                .build();
+
+        sectionService.save(newSectionRequestUm);
+        sectionService.save(newSectionRequestDois);
+
+        ResultActions response = mockMvc.perform(
+                get("/section")
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        response.andExpect(status().isFound());
     }
 }
