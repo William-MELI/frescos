7ad31ec9087ced401709aff68df0edb5f23f8631
illpereira <william.pereira@mercadolivre.com>
2022-11-21 11:58:50 -0300

    Refactor throable to exception

--- src/main/java/com/meli/frescos/exception/UsedPrimaryKeyConstraintException.java
+++ src/main/java/com/meli/frescos/exception/UsedPrimaryKeyConstraintException.java
@@ -1,6 +1,6 @@
 package com.meli.frescos.exception;
 
-public class UsedPrimaryKeyConstraintException extends Throwable {
+public class UsedPrimaryKeyConstraintException extends Exception {
     public UsedPrimaryKeyConstraintException(String message) {
         super(message);
     }
